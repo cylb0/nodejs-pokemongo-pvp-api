63964f46791265bd718274f8fe06c3ad7317c588
Morgan <morgan.foucaut@greta-formations.fr>
2023-09-28 07:57:59 +0200

    added favicon

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const favicon = require('serve-favicon')
 const app = express()
 const port = 3000
 
-app.use(morgan('dev'))
+app
+    .use(favicon(__dirname + '/src/img/favicon.ico'))
+    .use(morgan('dev')) 
 
 app.get('/', (req, res) => {
     res.send('API Pokemon GO')
