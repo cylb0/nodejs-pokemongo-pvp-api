dde1837298133d21d1c24b129434fc17bf074f14
Morgan <morgan.foucaut@greta-formations.fr>
2023-10-02 23:32:50 +0200

    Added dotenv dependency, added environment variables for database remote connexion.

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -3,8 +3,9 @@ const PokemonModel = require('./../models/pokemon')
 const UserModel = require('./../models/user')
 let pokemons = require('./../data/mock')
 
-const sequelize = new Sequelize('pokedex', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mariadb',
     dialectOptions: {
         timezone: 'Etc/GMT-2'
