85770f3036cbbfdb468b48f72dd0c3a2d63fd066
Morgan <morgan.foucaut@greta-formations.fr>
2023-10-02 12:52:30 +0200

    Added errors handling in login.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,12 +4,24 @@ const bcrypt = require('bcrypt')
 module.exports = (app) => {
     app.post('/api/login', (req, res) => {
         User.findOne({ where: { username: req.body.username } }).then(user => {
+
+            if (!user) {
+                const message = `This user doesn't exist.`
+                return res.status(404).json({ message  })
+            }
+
             bcrypt.compare(req.body.password, user.password).then(isValid => {
-                if(isValid) {
-                    const message = 'User has been logged in successfully.'
+                if(!isValid) {
+                    const message = 'The password is incorrect, please try again.'
                     return res.json({ message, data: user })
                 }
+                const message = 'User has been logged in successfully.'
+                return res.json({ message, data: user })
             })
+        })
+        .catch(error => {
+            const message = `User couldn't be authenticated. Please try again in a few minutes.`
+            return res.status(500).json({ message })
         })
     })
 }
